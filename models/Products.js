3ef5feee94f8747325e280d5b3acf4960c7065f2
Timibreez <ffftimi@gmail.com>
2022-08-29 11:26:17 +0100

    Product Model: added expires at

--- models/Products.js
+++ models/Products.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema({
     // img: {data: Buffer, type: String},
     price: {type: Number, required: true},
     quantity: {type: Number, required: true},
-    expiringDate: {type: Date}
+    expiringDate: {type: Date, default: new Date(), expires: 1440}
 },
 
 {timestamps: true}
